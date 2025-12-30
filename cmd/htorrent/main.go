f32a9a87a2904dc2f9423e3e52fd3eb7efec0403
Voornaamenachternaam <84537103+Voornaamenachternaam@users.noreply.github.com>
2025-12-30 14:41:38 +0100

    Update main.go

--- cmd/htorrent/main.go
+++ cmd/htorrent/main.go
@@ -1,6 +1,6 @@
 package main
 
-import "github.com/pojntfx/htorrent/cmd/htorrent/cmd"
+import "github.com/Voornaamenachternaam/htorrent/cmd/htorrent/cmd"
 
 func main() {
 	if err := cmd.Execute(); err != nil {
